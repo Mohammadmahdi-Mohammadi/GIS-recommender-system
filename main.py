a17ecfb3c57f74e90b1f4f6ebaaddfe5c33d80c9
Mohammad Andalibi <andalibimohammad@gmail.com>
2021-09-10 15:01:31 +0430

    Update main.py
    add recommend goal  by distance and Location status

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
  #Import Library
 import openrouteservice
 from openrouteservice import convert
-import json
+from folium.plugins import MarkerCluster
 import folium
 import xlrd
 map = folium.Map(location=[ 36.676056, 48.493992], zoom_start = 13)
 
 class object():
-    def __init__(self,  category, name,x, y, color ):
+    def __init__(self,  category, name,x, y, color,  status ):
         self.Category = category
         self.Name = name
         self.X = x 
         self.Y = y
         self.Color = color
+        self.Status = status
         
     def SetCategory(self, category):
         self.Category = Category
@@ -24,6 +25,8 @@ class object():
         self.Y = y
     def SetColor(self, color):
         self.Color = color
+    def SetStatus(self, status):
+        self.Status = status
     def GetCategory(self):
         return self.Category
     def GetName(self):
@@ -34,12 +37,8 @@ class object():
         return self.Y    
     def GetColor(self):
         return self.Color
-    
-    
-        
-#me = object('bank', 'maskan', 37.296933, -121.9574983 , ' hello')
-#print(me.Name)
-
+    def GetStatus(self):
+        return self.Status
 
 class recommenderSystem:
     
@@ -55,10 +54,10 @@ class recommenderSystem:
         #Create base map
         list = []
         #map = folium.Map(location=[ 36.676056, 48.493992], zoom_start = 13)
-        #print(    sheet.cell_value(10, 0))
+        
         for i in range(1, sheet.nrows):
-            
-            place = object(sheet.cell_value(i, 0), sheet.cell_value(i, 3) , sheet.cell_value(i, 2), sheet.cell_value(i, 1), sheet.cell_value(i, 4))
+           
+            place = object(sheet.cell_value(i, 0), sheet.cell_value(i, 3) , sheet.cell_value(i, 2), sheet.cell_value(i, 1), sheet.cell_value(i, 4),  sheet.cell_value(i, 5))
             
             list.append(place)
             #folium.Marker(location=[float(_y),float(_x)], popup = _name , icon=folium.Icon(color = _color )).add_to(map) 
@@ -66,23 +65,36 @@ class recommenderSystem:
         #Save the map
         map.save("map3.html")
         _user = User(sheet)
-        destinationX = sheet.cell_value(2, 15)
-        destinationY = sheet.cell_value(2, 14)
-        _user.goal(destinationX, destinationY)
+        whichCategory = input("enter What place do you want to go?")
+        _user.SelectBest(list,whichCategory )
+        if sheet.cell_value(2, 15) != 0:
+            destinationX = sheet.cell_value(2, 15)
+            destinationY = sheet.cell_value(2, 14)
+            _user.goal(destinationX, destinationY)
+        
+        
+        
+            
+        
         
     def main(self):
         sheet = self.ReadFile()
-        x = self.CreateMap(sheet)
+        self.CreateMap(sheet)
         
 
 class User(): 
+    #def __init__(self ):
     def __init__(self , sheet):
-       # print(sheet.cell_value(2, 13))
+        self.BestX=0
+        self.BestY=0
+        self.BestD=0
+        self.BestName = ''
         self.X= sheet.cell_value(2, 13)
         self.Y= sheet.cell_value(2, 12  )
         self.Name = sheet.cell_value(2, 6)
         self.Color = sheet.cell_value(2, 11)
-        folium.Marker(location=[ self.Y,self.X], popup =  self.Name , icon=folium.Icon(color = self.Color )).add_to(map) 
+        folium.CircleMarker(location=[self.Y, self.X], radius = 9, popup=self.Name, fill_color='black', color=self.Color, fill_opacity = 0.9).add_to(map)
+        #folium.Marker(location=[ self.Y,self.X], popup =  self.Name , icon=folium.Icon(color = self.Color )).add_to(map) 
         map.save("map3.html")
         
     def goal(self, destinationX, destinationY):
@@ -98,11 +110,31 @@ class User():
         #map = folium.Map(location=[ 36.676056, 48.493992],zoom_start=13, control_scale=True,tiles="cartodbpositron")
         folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+duration_txt,max_width=300)).add_to(map)
 
-        folium.Marker(location=list(coords[0][::-1]),popup="Galle fort",icon=folium.Icon(color="green"),).add_to(map)
+        folium.Marker(location=list(coords[0][::-1]),popup=self.Name,icon=folium.Icon(color="black"),).add_to(map)
 
-        folium.Marker(location=list(coords[1][::-1]),popup="Jungle beach",icon=folium.Icon(color="red"),).add_to(map)
+        folium.Marker(location=list(coords[1][::-1]),popup="Goal",icon=folium.Icon(color="white"),).add_to(map)
         map.save('map3.html')
-
+    def SelectBest(self ,  list ,  _category ):
+       
+        for i in list:
+            if i.Category ==_category :
+                client = openrouteservice.Client(key='5b3ce3597851110001cf62486d7b45f0c7424ffd9be3404ac5894fe1')
+                coords = ((self.X,self.Y),(i.X, i.Y))
+                res = client.directions(coords)
+                tempD = round(res['routes'][0]['summary']['distance']/1000,1) * i.Status            
+                if  self.BestD==0:
+                    self.BestD =tempD
+                else:
+                    if self.BestD>=tempD:
+                        
+                        self.BestD = tempD
+                        self.BestX = i.X
+                        self.BestY = i.Y
+                        self.BestName = i.Name
+             
+        print(self.BestName)
+        self.goal(self.BestX, self.BestY)
+        
 
 x = recommenderSystem()
 x.main()
